4110e35e0f95d05e1280e173e46b11d0c4c51bb9
smokeeaasd <lucasalencargomes111@gmail.com>
2024-09-18 11:09:08 -0300

    feat: setup organization routes
    - Added organization CRUD routes
    - Created Controllers, Requests and Resource classes.

---
+++ app/Http/Controllers/OrganizationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\OrganizationStoreRequest;
+use App\Http\Requests\OrganizationUpdateRequest;
+
+class OrganizationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(OrganizationStoreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(OrganizationUpdateRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -25,4 +26,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Rotas para o recurso de usuários
     Route::apiResource('users', UserController::class)
         ->only(['index', 'show']);
+
+    Route::apiResource('organizations', OrganizationController::class);
 });
